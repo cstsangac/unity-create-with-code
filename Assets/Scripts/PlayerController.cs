774d50a7b3eb0d8c924147da48477adb15a1b774
sing.tsang <sing.tsang@ha.org.hk>
2023-01-13 16:08:41 +0800

    Add player control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    public float velocityZ = 0.0f;
+    public float velocityZAdjustment = 10;
+    
+    public float rotateFactor = 100;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +19,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        velocityZ += Input.GetAxis("Vertical") / velocityZAdjustment;
         // we will move the vehicle forward
-        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+        transform.Translate(velocityZ * Time.deltaTime * Vector3.forward);
+        transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotateFactor * Time.deltaTime);
     }
 }
