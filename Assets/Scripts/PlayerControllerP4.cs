27dbf0a986ad85b8df6412d9763e0c7d28294967
sing.tsang <sing.tsang@ha.org.hk>
2023-01-17 15:20:24 +0800

    lesson 4.3

--- Assets/Scripts/PlayerControllerP4.cs
+++ Assets/Scripts/PlayerControllerP4.cs
@@ -1,18 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControllerP4 : MonoBehaviour
 {
   public float speed = 5.0f;
+  public float powerupStrength = 10;
+  public GameObject powerupIndicator;
+
+  public bool hasPowerup;
   public static GameObject self;
 
   Rigidbody rb;
   // Start is called before the first frame update
   void Start()
   {
+    Debug.Log("All Tags: " + string.Join(",", UnityEditorInternal.InternalEditorUtility.tags));
     rb = GetComponent<Rigidbody>();
-    self = this.gameObject;
+    self = gameObject;
   }
 
   // Update is called once per frame
@@ -20,9 +24,41 @@ public class PlayerControllerP4 : MonoBehaviour
   {
     if (transform.position.y < -5)
     {
-      Destroy(this.gameObject);
+      Destroy(gameObject);
     }
 
     rb.AddForce(Input.GetAxis("Vertical") * speed * RotateCamera.focalPoint.transform.forward);
+
+    powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.CompareTag("Powerup"))
+    {
+      StartCoroutine(nameof(PowerupCountdownRoutine));
+      Destroy(other.gameObject);
+    }
+
+  }
+
+  private void OnCollisionEnter(Collision other)
+  {
+    if (hasPowerup && other.gameObject.CompareTag("Enemy"))
+    {
+      Rigidbody enemyRb = other.gameObject.GetComponent<Rigidbody>();
+      Vector3 awayFromMe = other.gameObject.transform.position - transform.position;
+      enemyRb.AddForce(awayFromMe * powerupStrength, ForceMode.Impulse);
+
+    }
+  }
+
+  IEnumerator PowerupCountdownRoutine()
+  {
+    hasPowerup = true;
+    powerupIndicator.SetActive(true);
+    yield return new WaitForSeconds(5);
+    hasPowerup = false;
+    powerupIndicator.SetActive(false);
   }
 }
--- Assets/Scripts/SpawnManagerP4.cs
+++ Assets/Scripts/SpawnManagerP4.cs
@@ -6,6 +6,7 @@ public class SpawnManagerP4 : MonoBehaviour
 {
 
   public GameObject enemyPrefab;
+  public GameObject powerupPrefab;
   public float spawnFreq = 3;
   public float spawnRange = 9;
   // Start is called before the first frame update
@@ -16,9 +17,8 @@ public class SpawnManagerP4 : MonoBehaviour
 
   void Spawn()
   {
-    float x = Random.Range(-spawnRange, spawnRange);
-    float z = Random.Range(-spawnRange, spawnRange);
-    Instantiate(enemyPrefab, new Vector3(x, 6, z), enemyPrefab.transform.rotation);
+    Instantiate(enemyPrefab, new Vector3(Random.Range(-spawnRange, spawnRange), 6, Random.Range(-spawnRange, spawnRange)), enemyPrefab.transform.rotation);
+    Instantiate(powerupPrefab, new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange)), enemyPrefab.transform.rotation);
     Invoke(nameof(Spawn), Random.Range(1, spawnFreq));
   }
 }
