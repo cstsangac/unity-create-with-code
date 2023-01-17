6eb9a700481cf436861d9da499edb98d30ddc12c
sing.tsang <sing.tsang@ha.org.hk>
2023-01-17 11:02:33 +0800

    finish Challenge 3

---
+++ Assets/Challenge 3/Scripts/MoveLeftX3.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX3 : MonoBehaviour
+{
+  public float speed;
+
+  private float leftBound = -10;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (PlayerControllerX3.gameOver)
+    {
+      return;
+    }
+    // If game is not over, move to the left
+    transform.Translate(speed * Time.deltaTime * Vector3.left, Space.World);
+
+    // If object goes off screen that is NOT the background, destroy it
+    if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+    {
+      Destroy(gameObject);
+    }
+
+  }
+}
